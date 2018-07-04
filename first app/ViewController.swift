5b4753ca261d983f4d6cfad37ec92e5a158dfe8d
Tim Van Heester <timvanheester@macbook-air-van-tim.local>
2018-07-04 11:31:07 +0200

    Initial Commit

---
+++ first app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  first app
+//
+//  Created by Tim Van heester on 4/07/18.
+//  Copyright © 2018 Tim Van heester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
