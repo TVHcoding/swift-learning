a935f53146e7f01985fef4be342265d91beb8ed9
Tim Van heester <timvanheestercoding@gmail.com>
2018-07-06 13:13:09 +0200

    oefenapp start

--- first app/ViewController.swift
+++ first app/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var coollabel: UILabel!
+    
+    var gedrukt = 0
+    @IBAction func button(_ sender: Any) {
+    coollabel.text = "Hallo allemaal"
+        
+        
+        gedrukt = gedrukt + 1
+        
+        if gedrukt >= 10 {
+            coollabel.text = "Je hebt 10 keer op de knop gedrukt!!!!"}
+        if gedrukt >= 20 {
+            coollabel.text = "Je hebt 20 keer op de knop gedrukt!!!!"}
+        if gedrukt >= 30 {
+            coollabel.text = "Is het nog niet genoeg geweest"}
+        if gedrukt >= 35 {
+            coollabel.text = "Stop pushing me"}
+    }
+    
+    @IBAction func pushmealso(_ sender: Any) {
+    coollabel.text = "Leuk hé"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+
     }
 
 
