25fe26134e30eda17f4cd1c6bd2d49b07b63df2f
Tim Van heester <timvanheestercoding@gmail.com>
2018-07-06 14:16:04 +0200

    no message

--- first app/ViewController.swift
+++ first app/ViewController.swift
@@ -12,35 +12,21 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var coollabel: UILabel!
     
-    var gedrukt = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    
     @IBAction func button(_ sender: Any) {
-    coollabel.text = "Hallo allemaal"
-        
-        
-        gedrukt = gedrukt + 1
-        
-        if gedrukt >= 10 {
-            coollabel.text = "Je hebt 10 keer op de knop gedrukt!!!!"}
-        if gedrukt >= 20 {
-            coollabel.text = "Je hebt 20 keer op de knop gedrukt!!!!"}
-        if gedrukt >= 30 {
-            coollabel.text = "Is het nog niet genoeg geweest"}
-        if gedrukt >= 35 {
-            coollabel.text = "Stop touching me"}
-    }
-    
-    @IBAction func pushmealso(_ sender: Any) {
-    coollabel.text = "Leuk hé"
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
        
-
+        print(text1)
+        print(text1.text!)
+        print(text2.text!)
+        
     }
-
-
+    
 }
-
+    
+ 
+       
